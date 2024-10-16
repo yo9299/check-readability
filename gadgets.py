5cb5babde6fd7c63cccb9caaf8f05a8ec1f68fae
Virginia Ardevol Martinez <vardevol@PC-Ardevol.home>
2024-10-16 14:36:14 +0200

    test whole gadget

--- gadgets.py
+++ gadgets.py
@@ -4,9 +4,9 @@ import scipy
 import itertools
 from algorithm import algo
 
-def isRotation(tuple1, tuple2):
+def isRotation(tuple1, tuple2, rotations):
     n = len(tuple1)
-    for s in range(len(tuple1)):
+    for s in rotations : #range(len(tuple1)):#[6,12,18,24, 30]
         sol = True
         for i in range(len(tuple1)):
             if tuple1[i] != tuple2[(i+s)%(n)]:
@@ -15,18 +15,44 @@ def isRotation(tuple1, tuple2):
             return True 
     return False 
 
-def generateWeights(length, readability):
+
+def generateWeights(length, readability, rotations):
     result = []
     seen = set()  # Use a set to track unique combinations
 
     for combination in itertools.product(range(1, readability + 1), repeat=length):
         # Check if the combination or any of its rotations are already seen
-        if not any(isRotation(combination, existing) for existing in seen):
+        if not any(isRotation(combination, existing, rotations) for existing in seen):
             result.append(combination)  # Add the new unique combination
             seen.add(combination)  # Add to seen set to track rotations
     #print(len(result))
     return result
 
+def generateWeightsG(length, readability):
+    result = []
+    seen = set()  # Use a set to track unique combinations
+
+    for combination in itertools.product(range(1, readability + 1), repeat=length):
+        # Check if the combination or any of its rotations are already seen
+        if not any(equivalentGadgets(combination, existing) for existing in seen):
+            #result.append(combination)  # Add the new unique combination
+            seen.add(combination)  # Add to seen set to track rotations
+            print(seen)
+    #print(len(result))
+    return seen
+
+def equivalentGadgets(w1, w2):
+    c1 = w1[:6]
+    r1 = w1[-len(w1) +6 :]
+    c2 = w1[:6]
+    r2 = w1[-len(w1) +6 :]
+    for i in range(5):
+        sol = True 
+        if not isRotation(c1, c2, [i]) or not isRotation(r1, r2, [6*i]):
+            sol = False 
+        if sol:
+            return True
+    return False 
 # Create a directed graph
 G = nx.DiGraph()
 
@@ -39,7 +65,7 @@ target_nodes = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29]
 G.add_nodes_from(source_nodes, bipartite=0)  # Set of sources
 G.add_nodes_from(target_nodes, bipartite=1)  # Set of targets
 
-edges =[(0,1), (2,1), (2,3), (4, 3), (4,5), (0, 5), (0,7) , (6, 1), (6,7), (6,9), (8,7), (8,9), ( 10, 1), (2,11), (10, 11), (10,13), (12, 11), (12,13), (2,15), (14,3), (14,15), (16, 15), (14, 17), (16, 17), (18, 3), (4, 19), (18,19), (18, 21), (20, 19), (20,21), (4, 23), (22, 5), (22,23), (22,25), (24,23), (24,25), (26, 5), (0,27), (26,27), (26, 29), (28, 27), (28,29)]
+edges =[(0,1), (2,1), (2,3), (4, 3), (4,5), (0, 5), (0,7) , (6, 1), (6,7), (8,7),(6,9), (8,9), ( 10, 1), (2,11), (10, 11), (10,13), (12, 11), (12,13), (2,15), (14,3), (14,15), (16, 15), (14, 17), (16, 17), (18, 3), (4, 19), (18,19), (18, 21), (20, 19), (20,21), (4, 23), (22, 5), (22,23), (24,23), (22,25), (24,25), (26, 5), (0,27), (26,27), (26, 29), (28, 27), (28,29)]
 
 G.add_edges_from(edges)
 
@@ -89,4 +115,5 @@ def feasibleWeights(graph, readability):
           
 
 if __name__=="__main__":
-    feasibleWeights(C6, 3)
+    #feasibleWeights(C6, 3)
+    generateWeightsG(42,3)
